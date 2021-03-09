3d3729f2d4777063c1255dcb20766097551a811c
Ulf Samuelsson <ulf@emagii.com>
2021-03-09 10:03:04 +0100

    config.h
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

---
+++ src/include/config.h
@@ -0,0 +1,134 @@
+/* src/include/config.h.  Generated from config.h.in by configure.  */
+/* src/include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <algorithm> header file. */
+/* #undef HAVE_ALGORITHM */
+
+/* Define to 1 if you have the <assert.h> header file. */
+#define HAVE_ASSERT_H 1
+
+/* Define to 1 if you have the <deque> header file. */
+/* #undef HAVE_DEQUE */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the <fstream> header file. */
+/* #undef HAVE_FSTREAM */
+
+/* Define to 1 if you have the <functional> header file. */
+/* #undef HAVE_FUNCTIONAL */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <iomanip> header file. */
+/* #undef HAVE_IOMANIP */
+
+/* Define to 1 if you have the <iostream> header file. */
+/* #undef HAVE_IOSTREAM */
+
+/* Define to 1 if you have the <io.h> header file. */
+/* #undef HAVE_IO_H */
+
+/* Define to 1 if you have the <iterator> header file. */
+/* #undef HAVE_ITERATOR */
+
+/* Define to 1 if you have the <map> header file. */
+/* #undef HAVE_MAP */
+
+/* Define to 1 if you have the <memory> header file. */
+/* #undef HAVE_MEMORY */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <ostream> header file. */
+/* #undef HAVE_OSTREAM */
+
+/* Define to 1 if you have the <set> header file. */
+/* #undef HAVE_SET */
+
+/* Define to 1 if you have the <sstream> header file. */
+/* #undef HAVE_SSTREAM */
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdexcept> header file. */
+/* #undef HAVE_STDEXCEPT */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <string> header file. */
+/* #undef HAVE_STRING */
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <utility> header file. */
+/* #undef HAVE_UTILITY */
+
+/* Define to 1 if you have the <vector> header file. */
+/* #undef HAVE_VECTOR */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Description */
+/* #undef OS_LINUX */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "Ulf Samuelsson"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ielftool"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ielftool 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "ielftool-1.0"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://www.emagii.com/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Description */
+/* #undef PTHREAD */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
